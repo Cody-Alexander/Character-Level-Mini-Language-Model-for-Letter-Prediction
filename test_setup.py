15cad780d536ceda63bae96dd92c8a822b49ff3a
Cody Alexander <124945565+Cody-Alexander@users.noreply.github.com>
2025-10-21 10:53:55 +1300

    Delete test_setup.py

--- test_setup.py
+++
@@ -1,7 +0,0 @@
-import torch
-import numpy as np
-# verify versions
-print("Torch version:", torch.__version__)
-print("CUDA available:", torch.cuda.is_available())
-print("Device:", torch.device("cuda" if torch.cuda.is_available() else "cpu"))
-print("Numpy version:", np.__version__)
